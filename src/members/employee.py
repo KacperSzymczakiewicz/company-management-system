1887dc5134a670d6f42687fa412f093cc11b8402
Kacper Szymczakiewicz <kacperszymczakiewicz@gmail.com>
2026-07-04 16:59:33 +0200

    feat: add logging to Employee class

--- src/members/employee.py
+++ src/members/employee.py
@@ -1,9 +1,11 @@
+import logging
 from dataclasses import dataclass, field
 from typing import override
 
 from src.members.base import CompanyMember
 from src.models.task import Task
 
+logger = logging.getLogger(__name__)
 
 @dataclass
 class Employee(CompanyMember):
@@ -13,12 +15,18 @@ class Employee(CompanyMember):
         return f'Current tasks: {len(self.tasks)}'
 
     def add_task(self, task: Task) -> None:
+        logger.debug(f"Adding task: {task.task_id}")
         self.tasks[task.task_id] = task
+        logger.info(f"Task {task.task_id} added")
 
     def perform_task(self, task_id: int) -> None:
+        logger.debug(f"Checking existing task: task_id={task_id}")
         if task_id not in self.tasks:
+            logger.error(f"Task not found: task_id={task_id}")
             raise ValueError(f'Task {task_id} not found')
+        logger.debug(f"Task task_id={task_id} found")
         self.tasks[task_id].status = 'in progress'
+        logger.info(f"Task {task_id} in progress")
 
     @override
     def info(self) -> str:
