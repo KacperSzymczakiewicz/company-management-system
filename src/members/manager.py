55d2278be6652c2a31fb6ad8a8ee5f9e9f6af885
Kacper Szymczakiewicz <kacperszymczakiewicz@gmail.com>
2026-07-04 17:13:57 +0200

    feat: add logging to Manager

--- src/members/manager.py
+++ src/members/manager.py
@@ -1,3 +1,4 @@
+import logging
 from dataclasses import dataclass, field
 from typing import override
 
@@ -5,6 +6,8 @@ from src.members.base import CompanyMember
 from src.members.employee import Employee
 from src.models.task import Task
 from src.models.project import Project
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -16,15 +19,19 @@ class Manager(CompanyMember):
 
     def create_task_for_project(self, project: Project, task: Task) -> None:
         project.tasks[task.task_id] = task
+        logger.info(f"Manager created task: {task.task_id}")
 
     def review_task(self, task: Task) -> None:
         task.status = 'reviewed'
+        logger.info(f"Manager reviewed task: {task.task_id}")
 
     def complete_task(self, task: Task) -> None:
         task.status = 'completed'
+        logger.info(f"Manager closed task: {task.task_id}")
 
     def request_changes(self, task: Task) -> None:
         task.status = 'requires changes'
+        logger.info(f"Manager requested changes for task: {task.task_id}")
 
     @override
     def info(self) -> str:
