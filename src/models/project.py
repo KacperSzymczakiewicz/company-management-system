c5dd5c4cd3a5b3c2905f91ed82ee63ce280dbb1d
Kacper Szymczakiewicz <kacperszymczakiewicz@gmail.com>
2026-07-04 16:39:26 +0200

    feat: add logging to Project

--- src/models/project.py
+++ src/models/project.py
@@ -1,7 +1,9 @@
+import logging
 from dataclasses import dataclass, field
 
 from src.models.task import Task
 
+logger = logging.getLogger(__name__)
 
 @dataclass
 class Project:
@@ -11,6 +13,7 @@ class Project:
 
     def add_task(self, task: Task) -> None:
         self.tasks[task.task_id] = task
+        logger.info(f"Added task {task.task_id}: {task.title}")
 
     def info(self) -> str:
         return f'Name: {self.name}\nBudget: {self.budget}\nTasks:\n{self.tasks}'
