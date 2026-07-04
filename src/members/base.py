c9ecb169ded8c28e6506a8cf522968baf26bb049
Kacper Szymczakiewicz <kacperszymczakiewicz@gmail.com>
2026-07-04 16:52:14 +0200

    feat: add logging to CompanyMember class

--- src/members/base.py
+++ src/members/base.py
@@ -1,6 +1,8 @@
+import logging
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
 
+logger = logging.getLogger(__name__)
 
 @dataclass
 class CompanyMember(ABC):
@@ -10,6 +12,7 @@ class CompanyMember(ABC):
 
     def __post_init__(self) -> None:
         if self.member_id <= 0 or not self.name or self.age <= 0:
+            logger.error(f"Invalid member id: {self.member_id} or name: {self.name} or age: {self.age} !")
             raise ValueError("Invalid member data")
 
     @abstractmethod
