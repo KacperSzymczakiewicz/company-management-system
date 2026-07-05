d44e08e4432b6f9579ffe79319b77cade3ecbd48
Kacper Szymczakiewicz <kacperszymczakiewicz@gmail.com>
2026-07-05 17:57:24 +0200

    test: add Engineer tests

---
+++ tests/test_engineer.py
@@ -0,0 +1,17 @@
+import pytest
+
+from src.members.engineer import Engineer
+from src.models.task import Task
+
+
+class TestEngineer:
+
+    def test_invalid_task_id_raises_value_error(self) -> None:
+        with pytest.raises(ValueError):
+            engineer = Engineer(1, "Kacper", 20, specialization="backend")
+            engineer.implement_task(2)
+
+    def test_valid_task_change_status_implement_correctly(self, task: Task) -> None:
+        engineer = Engineer(1, "Kacper", 20, {1: task}, specialization="backend")
+        engineer.implement_task(1)
+        assert engineer.tasks[1].status == "implemented"
